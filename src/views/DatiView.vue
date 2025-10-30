c0f27ac6fe51df0ffa36e8cd77c000b602d55e56
alfonso.domenici@gmail.com <alfonso.domenici@gmail.com>
2025-10-30 15:50:34 +0100

    Add Vue Router integration and create basic views for navigation

---
+++ src/views/DatiView.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+  <h1>Dati View</h1>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const onViewDataClick = (e) => {
+  router.push('/dati');
+};
+</script>
+
+<template>
+  <h1>Login View</h1>
+  <button @click="onViewDataClick">Vai ai dati</button>
+</template>
