1dcbc0f8eb5ce019e50b7cf85db6914ef1fa2f42
alfonso.domenici@gmail.com <alfonso.domenici@gmail.com>
2025-11-03 11:10:30 +0100

    Implement data fetching and selection for Arduino in LoginView

--- src/views/DatiView.vue
+++ src/views/DatiView.vue
@@ -1,5 +1,16 @@
 <script setup>
+import { ref } from 'vue';
+
+const msg = ref('');
+
+const onChangeMessage = () => {
+  msg.value = `Messaggio cambiato alle ${new Date().toLocaleTimeString()}`;
+};
 </script>
 <template>
   <h1>Dati View</h1>
+
+  <p>Messaggio: {{ msg }}</p>
+
+  <button @click="onChangeMessage">Cambia Messaggio</button>
 </template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,14 +1,36 @@
 <script setup>
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
+const arduinos =ref([]);
+const selectedArduino = ref(null);
 const router = useRouter();
 
+fetch(`https://ard24lguerrini.pythonanywhere.com/api/arduinos/`, {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+    }
+})
+.then(response => response.json())
+.then(data => {
+    arduinos.value = data;
+})
+
+
 const onViewDataClick = (e) => {
+  console.log(`Arduino selezionato: ${selectedArduino.value}`);
   router.push('/dati');
 };
 </script>
 
 <template>
   <h1>Login View</h1>
+  <select v-model="selectedArduino">
+    <option v-for="arduino in arduinos" 
+      :key="arduino.id" :value="arduino.id">
+      {{ arduino.nome }}
+    </option>
+  </select>
   <button @click="onViewDataClick">Vai ai dati</button>
 </template>
