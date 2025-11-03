96aa5ddc42bb1ecd5df0cd2f94513bfb1ba2683d
alfonso.domenici@gmail.com <alfonso.domenici@gmail.com>
2025-11-03 11:55:36 +0100

    Refactor LoginView to handle Arduino selection and login process

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -14,13 +14,42 @@ fetch(`https://ard24lguerrini.pythonanywhere.com/api/arduinos/`, {
 })
 .then(response => response.json())
 .then(data => {
+    console.log(data);
     arduinos.value = data;
 })
 
+const isViewDataButtonDisabled = () => {
+  return selectedArduino.value === null;
+};
 
 const onViewDataClick = (e) => {
   console.log(`Arduino selezionato: ${selectedArduino.value}`);
-  router.push('/dati');
+  const macaddress = arduinos.value.
+    find(arduino => arduino.id === selectedArduino.value).macaddress;
+  console.log(`MAC Address: ${macaddress}`);
+  //effettua login
+  
+  fetch(`https://ard24lguerrini.pythonanywhere.com/api/auths`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+          "macaddress": macaddress
+      }),
+  })
+  .then(response => {
+    if (!response.ok) {
+        alert('Login failed: ' + response.statusText);
+        throw new Error('Login failed');
+    }
+    return response.json();
+  })
+  .then(data => {
+      console.log(data);
+      //naviga alla view dati
+      //router.push({ name: 'dati', params: { macaddress: macaddress } });
+  });
 };
 </script>
 
@@ -32,5 +61,8 @@ const onViewDataClick = (e) => {
       {{ arduino.nome }}
     </option>
   </select>
-  <button @click="onViewDataClick">Vai ai dati</button>
+  <button @click="onViewDataClick"
+    :disabled="isViewDataButtonDisabled()">
+    Vai ai dati
+  </button>
 </template>
